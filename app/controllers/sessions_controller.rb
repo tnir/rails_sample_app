aef14a7beb43bdd7cd5f500a82f8ef06d932a537
Michael Hartl <michael@michaelhartl.com>
2018-04-12 18:22:40 -0700

    Remove old comment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
